ed3ac1156d5e12277915d4c259e72f9873630dae
Emanuele Paiano <paiano.emanuele@gmail.com>
2017-04-23 13:10:11 +0200

    Add files via upload

--- guest/s/default/db/hotspot.sql
+++ guest/s/default/db/hotspot.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.2.12deb2+deb8u2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generato il: Mar 09, 2017 alle 00:51
--- Versione del server: 5.5.54-0ubuntu0.14.04.1
--- Versione PHP: 5.5.9-1ubuntu4.21
+-- Generation Time: Apr 22, 2017 alle 17:34
+-- Versione del server: 5.5.54-0+deb8u1
+-- PHP Version: 5.6.30-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,8 +17,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `hotspot`
+-- Database: `hotspot_backend`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `administrators`
+--
+
+CREATE TABLE IF NOT EXISTS `administrators` (
+`id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `administrators`
+--
+
+INSERT INTO `administrators` (`id`, `name`, `email`, `password`, `created`, `modified`) VALUES
+(8, 'admin', 'admin@backend.local', '$2y$10$g/Mop8PdCI5rBhdtzuFpge2HgVjZvCYcQtfNxgjj7gyJD.u6e5jTe', '2017-04-13 22:20:59', '2017-04-13 22:53:42');
 
 -- --------------------------------------------------------
 
@@ -27,16 +49,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `groups` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   `minutes` int(5) NOT NULL,
   `blockInterval` int(5) NOT NULL,
   `fakeminutesoffset` int(11) NOT NULL,
   `upload` int(11) NOT NULL,
   `download` int(11) NOT NULL,
-  `quota` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `quota` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `groups`
@@ -53,7 +74,7 @@ INSERT INTO `groups` (`id`, `name`, `minutes`, `blockInterval`, `fakeminutesoffs
 --
 
 CREATE TABLE IF NOT EXISTS `sessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `device` varchar(17) NOT NULL,
   `ip` varchar(15) NOT NULL,
   `ap` varchar(17) NOT NULL,
@@ -62,10 +83,15 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `remove` datetime NOT NULL,
   `browser` varchar(254) NOT NULL,
   `os` varchar(50) NOT NULL,
-  `user` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `device` (`device`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `device`, `ip`, `ap`, `lastlog`, `expire`, `remove`, `browser`, `os`, `user_id`) VALUES
+(5, 'Test Session', '192.168.1.1', '192.168.2.65', '2017-03-09 10:00:00', '2017-03-23 10:04:00', '2018-06-26 00:12:00', 'Firefox', 'Linux', 1);
 
 -- --------------------------------------------------------
 
@@ -74,19 +100,11 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 CREATE TABLE IF NOT EXISTS `settings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `param` varchar(20) NOT NULL,
   `value` varchar(50) NOT NULL,
-  `note` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
-
---
--- Dump dei dati per la tabella `settings`
---
-
-INSERT INTO `settings` (`id`, `param`, `value`, `note`) VALUES
-(1, 'nativeLang', 'it', 'default language'),
+  `note` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -95,22 +113,102 @@ INSERT INTO `settings` (`id`, `param`, `value`, `note`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(128) NOT NULL,
   `device` varchar(17) DEFAULT NULL,
-  `group_id` int(3) NOT NULL,
+  `group_id` int(11) NOT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `device`, `group_id`, `enabled`) VALUES
-(1, 'guest', '', '', 3, 1),
-(3, 'mario', 'staff', 'ignore', 2, 1);
+INSERT INTO `users` (`id`, `username`, `password`, `device`, `group_id`, `enabled`, `created`, `modified`) VALUES
+(1, 'mario', 'rossi', 'ignore', 2, 1, '2017-03-20 12:05:31', '2017-04-10 15:47:55'),
+(2, 'guest', 'password', 'ignore', 3, 1, '2017-03-20 12:13:38', '2017-04-10 15:49:30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrators`
+--
+ALTER TABLE `administrators`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `device` (`device`), ADD KEY `user_key` (`user_id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD KEY `group_key` (`group_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrators`
+--
+ALTER TABLE `administrators`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `sessions`
+--
+ALTER TABLE `sessions`
+ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Limiti per la tabella `users`
+--
+ALTER TABLE `users`
+ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
